47c859897dfc7c66b4205d285b88c9664d4b4a86
U-EMERGEDESK\Franck <franckbinard@gmail.com>
2020-03-03 19:14:04 -0500

    ui work

--- src/client/ui/ui.js
+++ src/client/ui/ui.js
@@ -4,16 +4,32 @@
 "use strict"
 /******************************************************************************/
 /******************************************************************************/
+
+const deviceRatios = [
+    {id: 1, ratio: '4x3'}, 
+    {id: 2, ratio: '16x9'}, 
+    {id: 3, ratio: '3x2'}
+]
+    
 const ui = (function(){
 
-    
+    let _windowHeight = null
+    let _windowWidth = null
+
+    let _screenDimensions = _ => {
+        _windowHeight = $( window ).height()
+        _windowWidth = $( window ).width()
+    }
+
     let _setHeight = (element, height) => {
         element.height(height)
     }
 
     let _configureUIElements = () => {
 
-        let windowHeight = $(window).height()
+        _screenDimensions()
+        debugger       
+
         let topNavHeight = $(window).height()/10
         _setHeight( $('#topNav'), topNavHeight ) 
 
@@ -22,11 +38,11 @@ const ui = (function(){
 
            
         $('#bottomNav').css({
-                    top: `${windowHeight - bottomNavHeight}px`, 
-                    left: '0'
+            top: `${_windowHeight - bottomNavHeight}px`, 
+            left: '0'
         })
 
-        let contentHeight = windowHeight - topNavHeight - bottomNavHeight 
+        let contentHeight = _windowHeight - topNavHeight - bottomNavHeight 
         _setHeight( $('#leftNav'), contentHeight) 
         $('#leftNav').css({
                     top: `${topNavHeight}px`, 
@@ -45,7 +61,10 @@ const ui = (function(){
 
     return {
 
-        configure : function( ){
+        configure : function( app, elements){
+            elements.forEach( element => {
+
+            })
             _configureUIElements()
             $(window).resize(()=> {
                 _configureUIElements()
@@ -58,14 +77,18 @@ const ui = (function(){
 
 const addUiFeature = app => {
 
-    ui.configure(app)
+    ui.configure( app )
+
     app.ui = ui
     app.addFeature({
         tag: 'ui', 
         state: 'implemented'
     })
+    require('./modal').addModalFeature( app, [
+            {}
+    ])
 
-    require('./modal').addModalFeature( app )
+
     return ui
 }
 
